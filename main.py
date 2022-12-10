2de60dfc10895d0735ccdd7263bc46bbd089500b
Vecta_ <59992068+Vecta6@users.noreply.github.com>
2022-12-10 18:21:01 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-chaine="10;31;31;14;4"
+a=0
+o=0
 
-binchaine=""
-for i in chaine.split(";"):
-    dec=int(i)
-    c=0
-    for q in range(0, 5):
-        p=(2**(4-q))
-        test=dec-p
-        if test>=0:
-            binchaine=binchaine+"1"
-            dec=test
-        elif test<=0:
-            binchaine=binchaine+"0"
-    binchaine=binchaine+";"
-    
-basic.show_string(binchaine)
+def decode(recchaine):
+    binchaine=""
+    for i in recchaine.split(";"):
+        dec=int(i)
+        c=0
+        for q in range(0, 5):
+            p=(2**(4-q))
+            test=dec-p
+            if test>=0:
+                binchaine=binchaine+"1"
+                dec=test
+            elif test<=0:
+                binchaine=binchaine+"0"
+        binchaine=binchaine+";"
+    binchaine=binchaine[0:-1]
+    return binchaine
+
+
+def on_received_string(receivedString):
+    global a, o
+    basic.clear_screen()
+    newbinchaine=decode(receivedString)
+    for i in newbinchaine.split(";"):
+        for j in i:
+            if j == "1":
+                led.plot(a, o)
+            a += 1
+        a = 0
+        o += 1
+radio.on_received_string(on_received_string)
+
+
+def on_button_pressed_a():
+    chaine="10;31;31;14;4"
+    radio.send_string(chaine)
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,23 +1,48 @@
-let dec: number;
-let c: number;
-let p: number;
-let test: number;
-let chaine = "10;31;31;14;4"
-let binchaine = ""
-for (let i of _py.py_string_split(chaine, ";")) {
-    dec = parseInt(i)
-    c = 0
-    for (let q = 0; q < 5; q++) {
-        p = 2 ** (4 - q)
-        test = dec - p
-        if (test >= 0) {
-            binchaine = binchaine + "1"
-            dec = test
-        } else if (test <= 0) {
-            binchaine = binchaine + "0"
+let a = 0
+let o = 0
+function decode(recchaine: string): string {
+    let dec: number;
+    let c: number;
+    let p: number;
+    let test: number;
+    let binchaine = ""
+    for (let i of _py.py_string_split(recchaine, ";")) {
+        dec = parseInt(i)
+        c = 0
+        for (let q = 0; q < 5; q++) {
+            p = 2 ** (4 - q)
+            test = dec - p
+            if (test >= 0) {
+                binchaine = binchaine + "1"
+                dec = test
+            } else if (test <= 0) {
+                binchaine = binchaine + "0"
+            }
+            
         }
-        
+        binchaine = binchaine + ";"
     }
-    binchaine = binchaine + ";"
+    binchaine = binchaine.slice(0, -1)
+    return binchaine
 }
-basic.showString(binchaine)
+
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    
+    basic.clearScreen()
+    let newbinchaine = decode(receivedString)
+    for (let i of _py.py_string_split(newbinchaine, ";")) {
+        for (let j of i) {
+            if (j == "1") {
+                led.plot(a, o)
+            }
+            
+            a += 1
+        }
+        a = 0
+        o += 1
+    }
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let chaine = "10;31;31;14;4"
+    radio.sendString(chaine)
+})
